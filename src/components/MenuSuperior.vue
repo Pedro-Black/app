ef5ab211e0c682a3ce62f916d423c2f5018bf7c4
Pedro-Black <phrod2007@gmail.com>
2024-03-06 14:16:35 -0300

    criação do arquivo MenuSuperior.vue e ListagemProduto.vue

---
+++ src/components/MenuSuperior.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue';
+
+import CartPlus from 'vue-material-design-icons/CartPlus.vue';
+import Account from 'vue-material-design-icons/Account.vue';
+import Menu from 'vue-material-design-icons/Menu.vue';
+
+const menuAberto = ref(false);
+</script>
+
+<template>
+    <header>
+      <div class="header--logo">
+        <img src="@/assets/logoFakeStore.png" alt="Logo" />
+        <h1>FakeStore</h1>
+      </div>
+      <nav>
+        <ul :class="menuAberto ? 'menu' : ''">
+          <li>Home</li>
+          <li>Eletrônicos</li>
+          <li>Jóias</li>
+          <li>Masculino</li>
+          <li>Feminino</li>
+        </ul>
+      </nav>
+      <div class="header--icons">
+        <Account />
+        <CartPlus />
+        <Menu class="menu-hamburger" @click="menuAberto = !menuAberto" />
+      </div>
+    </header>
+  </template>
